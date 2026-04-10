0bfbfbc390207060cc1606390ff190876c070c8b
Brigette <z5587785@ad.unsw.edu.au>
2026-04-11 00:05:09 +1000

    finished last share computation in controller class

--- party.py
+++ party.py
@@ -1,11 +1,12 @@
 # A proposed framework for representing parties and coordinators
 import secrets
+import threshold
 from abc import ABC, abstractmethod
 
 class Party:
   def __init__(self):
     # the random number each party holds
-    self.share = secrets.token_bytes(32)
+    self._share = secrets.token_bytes(32)
 
   @property
   def share(self):
@@ -13,17 +14,28 @@ class Party:
   
 
 class Controller:
-  def __init__(self, partyList: list[Party], secret: int):
+  # partyList: list of all n - 1 other parties, secret key is the 256 pairs from earlier
+  def __init__(self, partyList: list[Party], secret_key: list[list[int]], n: int):
     # the share is construct
-    self.share = self.__constructShare(partyList, secret)
+    self.share = self.__constructShare(partyList, secret_key, n)
 
-  def __constructShare(self, partyList: list[Party], secret: int):
+  def __constructShare(self, partyList: list[Party], secret_key: list[list[int]], n: int):
     # r_n = s1[0] r1r2  rn-1
-    share = secret
-    for x in partyList[0:]:
-      share ^= x
-    return share
-  
+
+    # get all the shares from before
+    shares = [party.share for party in partyList]
+
+    # this is the xor for our conttroller class
+    last_share = []
+    for i in range(256):
+        p0, p1 = secret_key[i][0], secret_key[i][1]
+        for j in range(n - 1):
+            p0 = threshold.xor_bytes(p0, shares[j])
+            p1 = threshold.xor_bytes(p1, shares[j])
+        last_share.append([p0, p1])
+    
+    return last_share
+
   @property
   def share(self):
     return self.share
