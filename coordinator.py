8901024c7b4c7fbb43aa38dabbc6624aa601feb2
Mehrab <mehrab.saad@gmail.com>
2026-04-11 13:10:18 +1000

    added implementation of add_party module

--- coordinator.py
+++ coordinator.py
@@ -1,9 +1,27 @@
 class Coordinator:
     # The coordinator manages the threshold signing workflow.
-    #It coordinates the parties but does not store the full secret key. 
+    # It coordinates the parties but does not store the full secret key.
     def __init__(self, public_key, parties=None):
-        # PK used to verify the final signature. 
+        # PK used to verify the final signature.
         self.public_key = public_key
 
-        # Parties - participants which hold each secret key pair. 
+        # Parties - participants which hold each secret key share.
         self.parties = list(parties or [])
+
+
+    def add_party (self, Party):
+        
+        # raise error if party is already added
+
+        if Party is self.parties:
+            raise ValueError("Party is already added")
+        
+        # raise error if Party is NONE
+        if Party is None:
+            raise ValueError("Party can't be None")
+        
+        
+
+        self.parties.append(Party)
+
+
