b294e3d00170955b3d986ff62b6b278bfe23e1e7
Mehrab <mehrab.saad@gmail.com>
2026-04-11 15:31:11 +1000

    collecting signature shares

--- coordinator.py
+++ coordinator.py
@@ -9,19 +9,32 @@ class Coordinator:
         self.parties = list(parties or [])
 
 
-    def add_party (self, Party):
-        
-        # raise error if party is already added
-
-        if Party is self.parties:
-            raise ValueError("Party is already added")
-        
+    def add_party(self, Party):
         # raise error if Party is NONE
         if Party is None:
             raise ValueError("Party can't be None")
-        
-        
 
+        # raise error if party is empty
+        if Party == "":
+            raise ValueError("Party can't be empty")
+
+        # raise error if party is already added
+        if Party in self.parties:
+            raise ValueError("Party is already added")
+        
         self.parties.append(Party)
 
+    def collect_sign_shares(self, message):
+        # To Store signature share returned by each of the parties
+        signature_shares = []
 
+        # Send the same message to every registered party.
+        for p in self.parties:
+            # Each party signs using its own secret-key share.
+            sig_share = p.sign(message)
+
+            # Add that party's signature share to the list.
+            signature_shares.append(sig_share)
+
+        # Return all collected signature shares to the coordinator.
+        return signature_shares
