92a4360ce74be42490bb77b0e8e2ac1a9a336185
Mehrab <mehrab.saad@gmail.com>
2026-04-20 22:30:08 +1000

    adding 'sucessful' parties test cases

--- test_coordinator.py
+++ test_coordinator.py
@@ -4,15 +4,35 @@ from coordinator import Coordinator
 
 
 class TestCoordinator(unittest.TestCase):
-
-    # Test: Adding empty Party to Coordinator : 
-    # expecTed result - valuerror
+    # Test: adding an empty party should raise ValueError.
     def test_add_party_empty(self):
         coordinator = Coordinator("public_key")
 
         with self.assertRaises(ValueError):
             coordinator.add_party("")
 
+    # Test: attempt to add a party which already exists in the coordinator
+    def test_existing_party(self):
+        coordinator = Coordinator("public_key")
+        coordinator.add_party("p1")
 
+        #trying to add existing party
+        with self.assertRaises(ValueError):
+            coordinator.add_party("p1")
+
+
+    # Test: Valid party added successfully 
+    def test_addParty_scess(self):
+        crd = Coordinator("public_key")
+        crd.add_party("p1")
+        self.assertIn("p1", crd.parties)
+    
+    # Test : different distinct parties can be added
+    def test_diff_parties(self):
+        crd = Coordinator("public_key")
+        crd.add_party("p1")
+        crd.add_party("p2")
+        # will be valid when length of added parties is 2
+        self.assertEqual(len(crd.parties), 2) 
 if __name__ == "__main__":
     unittest.main()
