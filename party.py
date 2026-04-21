e0b1e301c588ac7153870939b06c6e2a877c120d
Brigette <z5587785@ad.unsw.edu.au>
2026-04-22 00:39:54 +1000

    Fix: fixed party classes logic, enforcing ownership of random shares

--- party.py
+++ party.py
@@ -1,9 +1,11 @@
 from lamport import sign
-
+import secrets
 class Party:
 
     def __init__ (self, share):
-        self.share = share
+        """Initialises the party class. Each party holds their randomised uint32 share."""
+        self._share = secrets.token_bytes(32)
+
 
     def sign(self, message):
-        return sign(message, self.share)
+        return sign(message, self._share)
