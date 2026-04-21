17756ae71b4296bccee3156f5507a4aa1bea64a8
Mehrab <mehrab.saad@gmail.com>
2026-04-21 16:30:18 +1000

    Party & Coordinator interrogated into main

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
 import time
-from lamport import generate_keys, sign, verify
-from threshold import split_secret_key, combine_signatures
+from lamport import generate_keys
+from threshold import split_secret_key
+from party import Party
+from coordinator import Coordinator
+
 
 def main():
+    parties = []
     start_time = time.time()
     sk, pk = generate_keys()
     print(f"Key gen: {time.time() - start_time:.4f} seconds")
@@ -12,6 +16,17 @@ def main():
     n_parties = 3
     start_time = time.time()
     sk_shares = split_secret_key(sk, n_parties)
+
+    # Create Party-object for every shares
+    for s in sk_shares:
+        parties.append(Party(s))
+
+    # Then Register each of the party in coordinator
+    crd = Coordinator(pk)
+
+    for p in parties:
+        crd.add_party(p)
+    
     print(f"Share splitting: {time.time() - start_time:.4f} seconds")
 
     msg = input("Message to sign: ").strip()
@@ -19,16 +34,19 @@ def main():
         msg = "hello world I am in pain"
 
     start_time = time.time()
-    sig_shares = [sign(msg, share) for share in sk_shares]
-    print(f"Signing: {time.time() - start_time:.4f} seconds")
+    finalSign = crd.sign(msg)
+
+    #sig_shares = [sign(msg, share) for share in sk_shares]
+    print(f"Signing + Combining: {time.time() - start_time:.4f} seconds")
     
-    start_time = time.time()
-    combined_sig = combine_signatures(sig_shares)
-    print(f"Signature combination: {time.time() - start_time:.4f} seconds")
 
     start_time = time.time()
-    verify(msg, combined_sig, pk)
+    # Verify Final Signature against the Pk
+    res = crd.verify_Signature(msg, finalSign)
+    
     print(f"Verification: {time.time() - start_time:.4f} seconds")
+
+    print(f"Valid: {res}")
 
 if __name__ == "__main__":
     main()
