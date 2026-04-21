f89f628a500a57e81e67d18c25298ab05c56a615
Brigette <z5587785@ad.unsw.edu.au>
2026-04-22 02:16:46 +1000

    fix: corrected coordinator logic to closely follow implementation details (i.e. signature share defn)

--- coordinator.py
+++ coordinator.py
@@ -1,55 +1,56 @@
 import threshold
 import lamport
+import party
 
+import copy
+
+# In literature, this is the 'aggregator'
 class Coordinator:
     # The coordinator manages the threshold signing workflow.
     # It coordinates the parties but does not store the full secret key.
-    def __init__(self, public_key, parties=None):
+    
+    def __init__(self, public_key, parties=None, secret_array=None):
+        """
+        Constructor for coordinator
+        Stores the other parties, and uses the secret array to generate its own shares
+        If the share generation is to be moved outside of coordinator (i.e. accessor concerns),
+        all logic is in construct_coordinator_share
+        """
         # PK used to verify the final signature.
         self.public_key = public_key
 
         # Parties - participants which hold each secret key share.
         self.parties = list(parties or [])
 
+        # Share
+        self.share = self.construct_coordinator_share(secret_array)
 
-    def add_party(self, Party):
+
+    def add_party(self, p):
         # raise error if Party is NONE
-        if Party is None:
+        if p is None:
             raise ValueError("Party can't be None")
 
         # raise error if party is empty
-        if Party == "":
+        if p == "":
             raise ValueError("Party can't be empty")
 
         # raise error if party is already added
-        if Party in self.parties:
+        if p in self.parties:
             raise ValueError("Party is already added")
         
-        self.parties.append(Party)
-
-    def collect_sign_shares(self, message):
-        # Store the signature shares in an array , initially emptyu 
-        signature_shares = []
-
-        # Send the same message to every registered party
+        self.parties.append(p)
+    # coordinator is the final nth party, but as of now it requires you to construct the share
+    def construct_coordinator_share(self, secret_array):
+        # A sign share is simply, for a normal party, the randomised message they store
+        # secrets is a 256 length array of pairs
+        temp_arr = copy.deepcopy(secret_array)
+        
+        prefix_xor = 0
         for p in self.parties:
-            sig_share = p.sign(message)
+            prefix_xor ^= p.share
 
-            signature_shares.append(sig_share)
+        # The coordinator share: r_n= secret ^ r1 ^ r2 ^ ... ^ r_n - 1 
+        signature_shares = [[pair[0] ^ prefix_xor, pair[1] ^ prefix_xor] for pair in temp_arr]
 
         return signature_shares
-    
-    # Module for comboning all the shared-signaturesm to be used for final signature
-    def comb_sig_shares(self, sig_shares):
-        return threshold.combine_signatures(sig_shares)
-    
-    def sign (self, message):
-            # Collect all signatures ion the sig_sahre array & combine them to a final signature
-            sig_shares = self.collect_sign_shares(message)
-            final_signature = self.comb_sig_shares(sig_shares)
-            return final_signature
-    
-
-    # check the final combined signature is valid using our pUblic key 
-    def verify_Signature(self, message, signature):
-        return lamport.verify(message, signature, self.public_key)
--- party.py
+++ party.py
@@ -1,11 +1,5 @@
-from lamport import sign
 import secrets
 class Party:
-
     def __init__ (self):
         """Initialises the party class. Each party holds their randomised uint32 share."""
-        self._share = secrets.token_bytes(32)
-
-
-    def sign(self, message):
-        return sign(message, self._share)
+        self.share = secrets.token_bytes(32)
